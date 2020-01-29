57e9682115d2c0e1f2af41f56fdac40e58036543
Alexivkov <sivkov@inbox.ru>
2020-01-29 15:03:45 +0500

    adding color A6000

--- mario.js
+++ mario.js
@@ -3,10 +3,10 @@ printPyramid(10);
 
 function printPyramid(height) {
     var temp;
-    hashtag = "<span>#</span>";
+    hashtag = "<span id='square' >#</span>";
     for (let i = height; i > 0; i--) {
-        pre = "&nbsp;".repeat(i);
-        hashtag += "<span>#</span>";
+        pre = "<span>&nbsp;</span>".repeat(i);
+        hashtag += "<span id='square'>#</span>";
         //pre = pre.substring(0, pre.length - 1);
         temp = "<p>" + pre  + hashtag + "</p>";
         //console.log(temp);
