e068e735d69774822c544208802fd6313cd861f7
Alexivkov <sivkov@inbox.ru>
2020-01-29 14:00:43 +0500

    adding colored spans

--- mario.js
+++ mario.js
@@ -3,12 +3,12 @@ printPyramid(10);
 
 function printPyramid(height) {
     var temp;
-    hashtag = "#";
+    hashtag = "<span>#</span>";
     for (let i = height; i > 0; i--) {
         pre = "&nbsp;".repeat(i);
-        hashtag += "#";
+        hashtag += "<span>#</span>";
         //pre = pre.substring(0, pre.length - 1);
-        temp = "<p>" + pre + hashtag + "</p>";
+        temp = "<p>" + pre  + hashtag + "</p>";
         //console.log(temp);
         $("#pyramid").append(temp);
     }
